a6c0eb6464d8acb9295a2bba81bcea2d6d27e1a1
OneTwoTen <68427059+OneTwoTen@users.noreply.github.com>
2026-09-14 10:03:49 +0700

    merge(core): load contextual and dialog overlay styles

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,6 +1,7 @@
 import "./styles.css";
 import "./components/field-styles.css";
 import "./components/control-styles.css";
+import "./components/overlay-extras-styles.css";
 import "./overlay-footer.css";
 
 export * from "./component-metadata";
